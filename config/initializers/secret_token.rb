57c0c0a5053ffbb9e3fb866481827c07b74b9725
Sebastian Garcia <sdgarcia@gmail.com>
2012-10-01 20:13:39 -0300

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TangeretWebsite::Application.config.secret_token = '1f4e17ad48392299b39ef86858f134b954f168d38a21903bb7698fa8dd96c39be051ba566fd538b22c243d94f3ffeaa6cd3c39d36c028fb6b16cd173de20eebb'
